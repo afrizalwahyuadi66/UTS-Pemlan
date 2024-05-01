a8dc528e29e0cb22a347cd604fd33d7b74237763
Afrizal Wahyu Adi Putra <65825588+afrizalwahyuadi66@users.noreply.github.com>
2024-05-01 17:00:14 +0700

    Update and rename UTS-Pemlan.py to While-Do.cpp

---
+++ While-Do.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int count = 1;
+    while (count <= 10) {
+        std::cout << count << std::endl; // Mencetak nilai count
+        ++count; // Menambahkan count dengan 1
+    }
+    return 0;
+}
